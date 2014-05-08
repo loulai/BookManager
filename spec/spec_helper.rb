c8205680acb6b7f8ff902e49fd2f12cc1a6b66da
Louise <louiselai88@gmail.com>
2014-05-08 19:37:30 +0100

    added sample rspec tests just to prove that Datamapper behaves as we expected. All pass.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@
 
 ENV['RACK_ENV'] = 'test'
 
-require 'bookmanager'
+require_relative '../bookmanager'
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
